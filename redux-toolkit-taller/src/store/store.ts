89b28e57cff4323524a56bf6ea7745cdb360c4d6
githubfacu <elorzfacundo@gmail.com>
2024-07-19 05:05:27 -0300

    code

--- redux-toolkit-taller/src/store/store.ts
+++ redux-toolkit-taller/src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, Middleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth/authSlice";
 import { loggerMidelware } from "./middleware";
 
